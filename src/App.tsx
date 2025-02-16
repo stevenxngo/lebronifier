c0469d5640edf73c61a6d19fba5641e3a7304f9f
stevenxngo <83997026+stevenxngo@users.noreply.github.com>
2025-02-16 17:41:07 -0500

    Basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,10 @@ function App() {
 
   return (
     <div className="relative flex h-screen flex-col content-center bg-black/50 text-center text-white">
-      <h1 className="pt-8 text-5xl font-bold">LeBronifier</h1>
-      <div className="flex flex-row justify-center">
+      <h1 className="text-laker-purple pt-12 text-5xl font-bold drop-shadow-lg">
+        LeBronifier
+      </h1>
+      <div className="flex translate-y-24 flex-row justify-center md:translate-y-32 lg:translate-y-48">
         <input
           type="text"
           name="word"
@@ -28,16 +30,20 @@ function App() {
               handleLeword();
             }
           }}
-          className="mr-2 rounded bg-white p-1 text-black"
+          className="mr-3 w-56 rounded bg-white p-2 text-black"
         />
         <button
           onClick={() => handleLeword()}
-          className="rounded-md bg-purple-700 p-2 text-yellow-400 transition-all duration-100 ease-in hover:bg-purple-800"
+          className="bg-laker-purple text-laker-gold rounded-md p-3 transition-all duration-100 ease-in hover:bg-purple-950"
         >
           <FaCrown className="text-xl" />
         </button>
       </div>
-      {leword && <div className="text-2xl">{leword}</div>}
+      {leword && (
+        <div className="translate-y-32 text-2xl font-semibold md:translate-y-40 lg:translate-y-56">
+          {leword}
+        </div>
+      )}
     </div>
   );
 }
